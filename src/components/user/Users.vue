a2bd4568dc0e50f06ed1e595beef562b8b49c42e
huagjz <40297721+huagjz@users.noreply.github.com>
2021-01-17 18:11:30 +0800

    1-17
    添加、编辑、删除

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -27,10 +27,9 @@
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
-          <template>
-            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row)"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="delUser(scope.row)"></el-button>
             <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
               <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
             </el-tooltip>
@@ -49,11 +48,29 @@
       ></el-pagination>
     </el-card>
 
-    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
-      <span>这是一段信息</span>
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClose">
+      <!-- 内容主题区域 -->
+      <el-form :model="addForm" :rules="addFormrules" ref="addFormRef" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="用户名" prop="username"><el-input v-model="addForm.username"></el-input></el-form-item>
+        <el-form-item label="密码" prop="password"><el-input v-model="addForm.password" type="password"></el-input></el-form-item>
+        <el-form-item label="邮箱" prop="email"><el-input v-model="addForm.email"></el-input></el-form-item>
+        <el-form-item label="手机" prop="mobile"><el-input v-model="addForm.mobile"></el-input></el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog title="编辑用户信息" :visible.sync="editDialogVisible" width="50%" @close="editDialogClose">
+      <!-- 编辑用户信息 -->
+      <el-form :model="editForm" :rules="editFormrules" ref="aditFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username"><el-input v-model="editForm.username" disabled></el-input></el-form-item>
+        <el-form-item label="邮箱" prop="email"><el-input v-model="editForm.email"></el-input></el-form-item>
+        <el-form-item label="手机" prop="mobile"><el-input v-model="editForm.mobile"></el-input></el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -63,7 +80,50 @@
 export default {
   components: {},
   data() {
+    var checkMobile = (rule, value, callback) => {
+      var re = /^1[34578]\d{9}$/;
+
+      if (re.test(value)) {
+        return callback();
+      }
+
+      callback(new Error('请输入合法的手机号'));
+    };
     return {
+      editForm: {},
+      editDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      editFormrules: {
+        email: [
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      },
+      addFormrules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      },
       queryInfo: {
         query: '',
         pagenum: 1,
@@ -72,46 +132,102 @@ export default {
       userList: [],
       total: 0,
       addDialogVisible: false
-    };
+    }
   },
   created() {
-    this.getUserList();
+    this.getUserList()
   },
   methods: {
+    async delUser(item) {
+      const rsConfirm = await this.$confirm('确定要删除此用户吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err);
+      console.log(rsConfirm);
+      if (rsConfirm === 'cancel') return;
+      const { data: rs } = await this.$http.delete('users/' + item.id);
+      if (rs.meta.status !== 200) return this.$msg.error(rs.mate.msg);
+      this.$msg.success('删除成功');
+      this.getUserList();
+    },
+    editUser() {
+      this.$refs.aditFormRef.validate(async valid => {
+        if (!valid) return;
+        // 编辑用户的接口
+        const { data: rs } = await this.$http.put('users/' + this.editForm.id, { email: this.editForm.email, mobile: this.editForm.mobile });
+        console.log(rs);
+        if (rs.meta.status !== 200) return this.$msg.error(rs.meta.msg)
+        this.$msg.success('修改成功')
+        this.editDialogVisible = false;
+        this.getUserList();
+      });
+    },
+    async showEditDialog(item) {
+      console.log(item);
+      const { data: rs } = await this.$http.get('users/' + item.id);
+
+      if (rs.meta.status !== 200) return this.$msg.error(rs.mate.msg);
+      console.log(rs);
+      this.editForm = rs.data;
+      this.editDialogVisible = true;
+    },
+    editDialogClose() {
+      this.$refs.aditFormRef.resetFields();
+    },
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return;
+
+        // 添加用户的接口
+        const { data: rs } = await this.$http.post('users', this.addForm);
+        console.log(rs);
+        if (rs.meta.status !== 201) return this.$msg.error(rs.meta.msg)
+
+        this.$msg.success('添加用户成功')
+        this.addDialogVisible = false;
+        this.getUserList();
+      });
+    },
+    addDialogClose() {
+      this.$refs.addFormRef.resetFields();
+    },
     // 用户状态
     async userStateChange(userInfo) {
-      console.log(userInfo);
-      const { data: rs } = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`);
+      console.log(userInfo)
+      const { data: rs } = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
 
       if (rs.meta.status !== 200) {
-        this.$msg.error(rs.meta.msg);
-        userInfo.mg_state = false;
-        return;
+        this.$msg.error(rs.meta.msg)
+        userInfo.mg_state = false
+        return
       }
-      this.$msg.success('更新用户状态成功！');
+      this.$msg.success('更新用户状态成功！')
     },
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
-      console.log();
-      this.queryInfo.pagesize = val;
-      this.getUserList();
+      console.log(`每页 ${val} 条`)
+      console.log()
+      this.queryInfo.pagesize = val
+      this.getUserList()
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
-      this.queryInfo.pagenum = val;
-      this.getUserList();
+      console.log(`当前页: ${val}`)
+      this.queryInfo.pagenum = val
+      this.getUserList()
     },
     // 获取数据列表
     async getUserList() {
-      const { data: rs } = await this.$http.get('users', { params: this.queryInfo });
+      const { data: rs } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
 
-      console.log(rs);
-      if (rs.meta.status !== 200) return this.$msg.error(rs.meta.msg);
-      this.userList = rs.data.users;
-      this.total = rs.data.total;
+      console.log(rs)
+      if (rs.meta.status !== 200) return this.$msg.error(rs.meta.msg)
+      this.userList = rs.data.users
+      this.total = rs.data.total
     }
   }
-};
+}
 </script>
 
 <style></style>
